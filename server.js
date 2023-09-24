20ab1d0da629dc72d9a5661922d6e25faff205c8
Joshua Lai <josh.lai@csu.fullerton.edu>
2023-09-23 20:35:54 -0700

    Add routes folder and default route

--- server.js
+++ server.js
@@ -55,9 +55,11 @@ app.use('/api', router);
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+app.use('/', require('./routes/index'));
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'views/index.html'));
+// });
 
 // START THE SERVER
 // =============================================================================
