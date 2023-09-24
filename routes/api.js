0a53e4459a4aa4c631adbbaee7f00e7a098fe74d
Joshua Lai <josh.lai@csu.fullerton.edu>
2023-09-23 22:30:08 -0700

    Move database query to api route

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const router = express.Router();    
 const path = require('path');
+var pool = require('../config/db.js')
 
 router.get('/', function (req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
@@ -9,6 +10,27 @@ router.get('/', function (req, res) {
 router.get('/c', function (req, res) {
     res.json({ message: 'Get request worked. /c route' });
   });
+
+router.get('/select', function (req, res) {
+  pool.query('SELECT NOW()', (err, res) => {
+    console.log(res.rows)
+    // pool.end()
+  });
+
+  add = `INSERT INTO teas (teaname, teatype, temperature, mood, caffeinated) 
+  VALUES
+      ('Jasmine', 'Green', 'Hot', 'Relax', TRUE),
+      ('English Breakfast', 'Black', 'Hot', 'Relax', TRUE),
+      ('Alishan', 'Oolong', 'Hot', 'Relax', TRUE);`
+
+  // pool.query(add);
+  pool.query('SELECT * FROM teas', (err, res) => {
+    console.log(res.rows)
+    // pool.end()
+  });
+  
+  res.json({ message: 'This is the /select route.' });
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var path = require('path');
-// var client = require('./db.js')
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -14,38 +13,10 @@ var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+// var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-// router.get('/', function (req, res) {
-//   res.json({ message: 'hooray! welcome to our api!' });
-// });
-// router.get('/c', function (req, res) {
-//   res.json({ message: 'Get request worked. /c route' });
-// });
-router.get('/ab', function (req, res) {
-  client.query('SELECT NOW()', (err, res) => {
-    console.log(res.rows)
-    // client.end()
-  });
-  text = `CREATE TABLE [IF NOT EXISTS] accounts (
-    user_id serial PRIMARY KEY,
-    username VARCHAR ( 50 ) UNIQUE NOT NULL,
-    password VARCHAR ( 50 ) NOT NULL,
-    email VARCHAR ( 255 ) UNIQUE NOT NULL,
-    created_on TIMESTAMP NOT NULL,
-          last_login TIMESTAMP 
-  );`
-  add = `INSERT INTO accounts (username, password, email, created_on)
-    VALUES('James', '12345', 'james@example.com', '2016-06-22 19:10:25-07');`
-  // client.query(text);
-  // client.query(add);
-  client.query('SELECT * FROM accounts', (err, res) => {
-    console.log(res.rows)
-    client.end()
-  });
-  res.json({ message: 'This is the /ab route.' });
-});
+
 
 // more routes for our API will happen here
 
