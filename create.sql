0212872128c30443706315b31f5b7172fb8728d2
Joshua Lai <josh.lai@csu.fullerton.edu>
2023-09-23 17:40:23 -0700

    Create sample tea database

--- create.sql
+++ create.sql
@@ -1,18 +1,18 @@
 CREATE DATABASE tea_db;
 \c tea_db
 
-CREATE TABLE IF NOT EXISTS accounts (
-    user_id serial PRIMARY KEY,
-    username VARCHAR ( 50 ) UNIQUE NOT NULL,
-    password VARCHAR ( 50 ) NOT NULL,
-    email VARCHAR ( 255 ) UNIQUE NOT NULL,
-    created_on TIMESTAMP NOT NULL DEFAULT NOW(),
-    last_login TIMESTAMP 
+CREATE TABLE IF NOT EXISTS teas (
+    tea_id serial PRIMARY KEY,
+    teaname VARCHAR ( 50 ) NOT NULL,
+    teatype VARCHAR ( 50 ) NOT NULL,
+    temperature VARCHAR ( 50 ) NOT NULL,
+    mood VARCHAR ( 50 ) NOT NULL,
+    caffeinated BOOLEAN NOT NULL
 );
 
-INSERT INTO accounts (username, password, email)
+INSERT INTO teas (teaname, teatype, temperature, mood, caffeinated)
     VALUES
-        ('James', '12345', 'james@example.com'),
-        ('Amy', '12345', 'amy@example.com'),
-        ('Link', '12345', 'link@example.com');
+        ('Jasmine', 'Green', 'Hot', 'Relax', TRUE),
+        ('English Breakfast', 'Black', 'Hot', 'Relax', TRUE),
+        ('Alishan', 'Oolong', 'Hot', 'Relax', TRUE);
 
