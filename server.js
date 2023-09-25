2ca6f6a536ec7ba47bb1f42a0ad9aaf015bb2ae0
Joshua Lai <josh.lai@csu.fullerton.edu>
2023-09-25 11:46:12 -0700

    Remove body-parser module

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 const express = require('express');
 // call express
 const app = express(); // define our app using express
-const bodyParser = require('body-parser');
 const path = require('path');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = process.env.PORT || 8080; // set our port
 
