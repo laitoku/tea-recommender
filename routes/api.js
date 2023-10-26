9e98346e59ff8c66abf0a93b613fbb8a4594cfc8
Joshua Lai <josh.lai@csu.fullerton.edu>
2023-10-26 14:32:38 -0700

    Remove unused code

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const control = require('../controllers/index');
+const control = require('../controllers/tea_controller');
 
 router.get('/', (req, res) => {
   res.json({ message: 'hooray! welcome to our api!' });
@@ -16,32 +16,5 @@ router.route('/select')
   .post(control.getTea);
 router.route('/sel')
   .get(control.getTea);
-// function (req, res) {
-//   // pool.query('SELECT NOW()', (err, res) => {
-//   //   console.log(res.rows)
-//   //   // pool.end()
-//   // });
-
-//   add = `INSERT INTO teas (teaname, teatype, temperature, mood, caffeinated)
-//   VALUES
-//       ('Jasmine', 'Green', 'Hot', 'Relax', TRUE),
-//       ('English Breakfast', 'Black', 'Hot', 'Relax', TRUE),
-//       ('Alishan', 'Oolong', 'Hot', 'Relax', TRUE);`
-
-//   // pool.query(add);
-//   // var result = pool.query('SELECT * FROM teas', (err, res) => {
-//   //   // console.log(res.rows)
-//   //   var result = JSON.stringify(res.rows);
-//   //   // pool.end()
-//   // });
-//   model.selectAllTea().then((result) => {
-//     // console.log(result);
-//     var tea = JSON.stringify(result, null, 4)
-//     // var tea = tea.replace(/\n/g, '\n')
-//     // console.log(tea)
-//     res.end(tea)
-//   });
-//   // res.json({ message: 'This is the /select route.' });
-// });
 
 module.exports = router;
